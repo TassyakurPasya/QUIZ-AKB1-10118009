d5bc725a359632cbade98d34f73c3ac3ccd9a57e
Achapasya2109 <acha.pasya2109@gmail.com>
2021-05-29 12:50:07 +0700

    bismillah

---
+++ app/src/main/java/com/example/quiz10118009/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.quiz10118009;
+//29 mei 2021, 10118009, Tassyakur Pasya, IF-01
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
